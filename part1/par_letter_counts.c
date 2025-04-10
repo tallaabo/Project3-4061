fa1a8faf61a4940a95b7f80a9b4b99d0df48a19d
yabi <youssefabdulle12@gmail.com>
2025-04-10 09:40:25 +0000

    finished task 1

--- part1/par_letter_counts.c
+++ part1/par_letter_counts.c
@@ -18,6 +18,20 @@
  * Returns 0 on success or -1 on error.
  */
 int count_letters(const char *file_name, int *counts) {
+    FILE *file = fopen(file_name, "r");
+    if (!file) {
+        perror("fopen");
+        return -1;
+    }
+
+    int c;
+    while ((c = fgetc(file)) != EOF) {
+        if (isalpha(c)) {
+            counts[tolower(c) - 'a']++;
+        }
+    }
+
+    fclose(file);
     return 0;
 }
 
@@ -30,6 +44,12 @@ int count_letters(const char *file_name, int *counts) {
  * Returns 0 on success or -1 on error
  */
 int process_file(const char *file_name, int out_fd) {
+    int counts[ALPHABET_LEN] = {0};
+    if (count_letters(file_name, counts) == -1)
+        return -1;
+
+    if (write(out_fd, counts, sizeof(counts)) != sizeof(counts))
+        return -1;
     return 0;
 }
 
@@ -39,13 +59,50 @@ int main(int argc, char **argv) {
         return 0;
     }
 
-    // TODO Create a pipe for child processes to write their results
-    // TODO Fork a child to analyze each specified file (names are argv[1], argv[2], ...)
-    // TODO Aggregate all the results together by reading from the pipe in the parent
+    int num_files = argc - 1;
+    int pipe_fds[2];
+    if (pipe(pipe_fds) == -1) {
+        perror("pipe");
+        return 1;
+    }
 
-    // TODO Change this code to print out the total count of each letter (case insensitive)
+    pid_t pids[num_files];
+    for (int i = 1; i <= num_files; i++) {
+        pid_t pid = fork();
+        if (pid == -1) {
+            perror("fork");
+            return 1;
+        } else if (pid == 0) {
+            close(pipe_fds[0]);
+            if (process_file(argv[i], pipe_fds[1]) == -1) {
+                // Let count_letters print fopen error, just exit silently
+                exit(1);
+            }
+            exit(0);
+        }
+        pids[i - 1] = pid;
+    }
+
+    close(pipe_fds[1]);
+
+    int total_counts[ALPHABET_LEN] = {0};
+    for (int i = 0; i < num_files; i++) {
+        int status;
+        waitpid(pids[i], &status, 0);
+        if (WIFEXITED(status) && WEXITSTATUS(status) == 0) {
+            int temp_counts[ALPHABET_LEN];
+            if (read(pipe_fds[0], temp_counts, sizeof(temp_counts)) != sizeof(temp_counts)) {
+                perror("read");
+                return 1;
+            }
+            for (int j = 0; j < ALPHABET_LEN; j++) {
+                total_counts[j] += temp_counts[j];
+            }
+        }
+    }
+
     for (int i = 0; i < ALPHABET_LEN; i++) {
-        printf("%c Count: %d\n", 'a' + i, -1);
+        printf("%c Count: %d\n", 'a' + i, total_counts[i]);
     }
     return 0;
 }
